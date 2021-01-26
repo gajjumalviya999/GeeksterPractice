5fc9624a26382d2a88eefe6f52395885704a11b5
gajjumalviya999 <gajjumalviya999@gmail.com>
2021-01-26 18:30:09 +0530

    start interview preparation by aman sir

---
+++ src/interviewprep600/KCON.java
@@ -0,0 +1,28 @@
+package interviewprep600;
+class KCON{
+	public static void main(String[] args) {
+		int a[]= {1,-2,1};
+		int k=2;
+		System.out.println(kcon(a,k));
+	}
+
+	private static int kcon(int[] a, int k) {
+		// TODO Auto-generated method stub
+		int maxsum=Integer.MIN_VALUE;
+		int B[]=new int[a.length*k];
+		int N=a.length;
+		for(int i=0;i<N*k;i++) {
+			B[i]=a[i%N];
+		}
+		int currsum=0;
+		for(int i=0;i<N*k;i++) {
+			currsum+=B[i];
+			if(currsum>maxsum)
+				maxsum=currsum;
+			if(currsum<0)
+				currsum=0;
+		
+		}
+		return maxsum;
+	}
+}
---
+++ src/interviewprep600/subarraywithsum.java
@@ -0,0 +1,31 @@
+package interviewprep600;
+class subarraywithsum{
+	public static void main(String[] args) {
+		int arr[]={1, 4};
+		function1(arr,0);
+		
+	}
+
+	private static void function1(int[] arr, int sum) {
+		// TODO Auto-generated method stub
+		int currsum=0;
+		int flag=0;
+		for(int i=0;i<arr.length;i++) {
+			currsum=0;
+			for(int j=i;j<arr.length;j++) {
+				currsum+=arr[j];
+				if(currsum==sum) {
+					System.out.println("element found between index " + i+  " and " +j);
+					flag=1;
+					break;
+				}
+			}
+			if(flag==1)
+				break;
+		}
+		if(flag==0)
+			System.out.println("No subarray found\r\n" + 
+					"There is no subarray with " + sum + "sum");
+		
+	}
+}
