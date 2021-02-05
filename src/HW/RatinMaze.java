2ebd52f7c37b0ece33bcc7f938ff2eda95d2ea04
gajjumalviya999 <gajjumalviya999@gmail.com>
2021-02-05 22:50:53 +0530

    Rat in Maze

---
+++ src/HW/RatinMaze.java
@@ -0,0 +1,36 @@
+package HW;
+class RatinMaze{
+	public static void main(String[] args) {
+		int arr[][]={{1, 0, 0, 0},
+					 {1, 1, 0, 1},
+					 {0, 1, 0, 0},
+					 {1, 1, 1, 1}};
+		int sol[][]=new int [arr.length][arr[0].length];
+	   ratInMaze(arr,0,0,sol);
+	 
+}
+
+	private static void ratInMaze(int[][] arr,int cur_r,int cur_c,int sol[][]) {
+		// TODO Auto-generated method stub
+		if(cur_r==arr.length-1&&cur_c==arr[0].length-1) {
+			sol[cur_r][cur_c]=1;
+			 for(int i[]:sol) {
+				   for(int j=0;j<i.length;j++)
+					   System.out.print(i[j]+"\t" );
+				   System.out.println();
+			 }
+			return;
+		}
+		
+		if(arr[cur_r][cur_c]==1) {
+			sol[cur_r][cur_c]=1;
+			if(cur_c+1<arr.length)
+				ratInMaze(arr,cur_r,cur_c+1,sol);
+			if(cur_r+1<arr.length)
+				ratInMaze(arr,cur_r+1,cur_c,sol);
+			sol[cur_r][cur_c]=0;
+		}
+		else
+			return;	
+	}	
+}
