3b528a5930ff17f2f09344af54a242051f676d3b
gajjumalviya999 <gajjumalviya999@gmail.com>
2021-02-27 23:38:00 +0530

    Circular Tour Path Petrol Pump

--- src/stack/CircularTourPetrolPump.java
+++ src/stack/CircularTourPetrolPump.java
@@ -9,7 +9,7 @@ class petrolpump{
 }
 public class CircularTourPetrolPump {
 	public static void main(String[] args) {
-		petrolpump arr[]= { new petrolpump(6,4),new petrolpump(3,6), new petrolpump(7,3)};
+		petrolpump arr[]= { new petrolpump(4,6),new petrolpump(6,5), new petrolpump(7,3), new petrolpump(4,5)};
 		CircularTourPetrolPump cp= new CircularTourPetrolPump();
 		System.out.println(cp.firstpoint(arr));
 	}
@@ -20,10 +20,12 @@ public class CircularTourPetrolPump {
 		int c=0;
 		int balance=0;
 		int r=arr.length-1;
-		while(r!=c) {
+		
+		while(c!=r) {
+			
 			balance=arr[c].fuel-arr[c].distance;
 			if(balance<0) {
-				f=c;
+				f=c+1;
 				if(f==0)
 					r=arr.length-1;
 				else
@@ -35,6 +37,7 @@ public class CircularTourPetrolPump {
 					c=0;
 		}
 		}
+		
 		return f;
 		
 	}
